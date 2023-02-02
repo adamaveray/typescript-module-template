2ad4846dfa80620eca3ce588b68129732dc1e13a
Adam Averay <adam@averay.com>
2023-02-02 17:27:08 +0900

    Resolve test ESLint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
 import { makeEslintConfig } from '@averay/codeformat';
+import globals from 'globals';
 
 export default [
   {
     ignores: ['dist/**/*'],
+  },
+  {
+    files: ['test/**/*'],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest, NodeJS: 'readonly' },
+    },
   },
   ...makeEslintConfig({ tsconfigPath: './tsconfig.json' }),
 ];
