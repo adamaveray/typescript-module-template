e9a277c311f5e4d5166229b34cc019b736acae2c
Adam Averay <adam@averay.com>
2023-03-06 04:37:26 +0900

    Improve default ESLint rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,22 @@ export default [
   {
     ignores: ['dist/**/*'],
   },
+  ...makeEslintConfig({ tsconfigPath: './tsconfig.json' }),
+  {
+    files: ['src/**/*'],
+    languageOptions: {
+      // globals: { ...globals.node, NodeJS: 'readonly' },
+      // globals: globals.browser,
+    },
+  },
   {
     files: ['test/**/*'],
     languageOptions: {
       globals: { ...globals.node, ...globals.jest, NodeJS: 'readonly' },
     },
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+    },
   },
-  ...makeEslintConfig({ tsconfigPath: './tsconfig.json' }),
 ];
